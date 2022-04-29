5729886a4834907b3ce6d2cd51d10042df60f33f
LuisQuin <plissken2013es@gmail.com>
2022-04-30 00:16:02 +0200

    Load/Save game

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-$(function() {
+function startDraggable() {
     $(".draggable" ).draggable({
         helper: "clone",
         cursor: "move",
         stop: function(event, ui) {
             let kk = $(ui.helper)
                 .clone(true)
-                .removeClass('box ui-draggable ui-draggable-dragging')
+                .removeClass('box draggable ui-draggable ui-draggable-dragging')
                 .addClass('box-clone')
-                .appendTo('body');
+                .appendTo('#page');
             kk.draggable({
                 distance: 50,
                 helper: "clone",
@@ -18,7 +18,28 @@ $(function() {
             });
         }
     });
+    $(".box-clone").draggable({
+        distance: 50,
+        helper: "clone",
+        cursor: "move",
+        stop: function(event, ui) {
+            $(event.target).remove();
+        }
+    });
     $(".draggable-turn").draggable({
         cursor: "move"
     });
+};
+
+$(function() {
+    startDraggable();
  });
+
+$("#save").click(function() {
+	localStorage.setItem('saved_game', $("#page").html());
+});
+
+$("#load").click(function() {
+    $("#page").html(localStorage.getItem('saved_game'));
+    startDraggable();
+});
